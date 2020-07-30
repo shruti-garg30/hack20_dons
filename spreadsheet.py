5b0cb0b3ee0ee556a20586e43d9ab4a80a086e20
shruti-garg30 <shrutigarg1999@gmail.com>
2020-07-30 20:24:03 +0530

    Tabs for different domains will open

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,4 +1,5 @@
 import gspread
+import webbrowser
 from oauth2client.service_account import ServiceAccountCredentials
 
 # use creds to create a client to interact with the Google Drive API
@@ -13,6 +14,21 @@ client = gspread.authorize(creds)
 sheet = client.open("email sample").sheet1
 
 # Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-#print ("abc")
-print (list_of_hashes)
+email_list = sheet.get_all_values()
+#print (list_of_hashes)
+def remove_till_at(the_list):
+    a=[]
+    for x in the_list:
+        for y in x:
+            a.append(y.split('@'))
+    b=[]
+    for x in a:
+        b.append(x[1])
+    b1= 'http://'
+    b1 += '{0}'
+    b= [b1.format(i) for i in b]
+    return b
+c= remove_till_at(email_list)
+#print(c[:5])
+for x in c:
+    webbrowser.open_new_tab(x)
