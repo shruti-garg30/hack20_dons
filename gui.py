1a0bc003c93ddcc5fde6fc71e1df6494a5353eef
shruti-garg30 <shrutigarg1999@gmail.com>
2020-07-31 05:39:05 +0530

    updated gui with select all

--- gui.py
+++ gui.py
@@ -6,10 +6,16 @@ class MyWindow:
         data=("abc.com","xyz.com")
         for num in data:
             self.lb.insert(END,num)
-        self.btn1 = Button(win, text='Search the web')
-        self.lb.place(x=100, y=50)
-        self.b1=Button(win, text='Search the web', command=self.search)
-        self.b1.place(x=100, y=150)
+        self.btn1 = Button(win, text='Select All')
+        self.btn2 = Button(win, text='Search the web')
+        self.lb.place(x=150, y=50)
+        self.b1=Button(win, text='Select All', command=self.select)
+        self.b2=Button(win, text='Search the web', command=self.search)
+        self.b1.place(x=100, y=200)
+        self.b2.place(x=200, y=200)
+    def select(self):
+        #perform operation
+        webbrowser.open_next_tab("https://google.com")
     def search(self):
         webbrowser.open_next_tab("https://google.com")
         #oshi add function here
